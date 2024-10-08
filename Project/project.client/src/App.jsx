2356a44c1b57f5484b829951cde8a05051f49060
Clément BADRONE <clement.badrone@epitech.eu>
2024-10-08 09:53:06 +0200

    Setup Login Page

--- Project/project.client/src/App.jsx
+++ Project/project.client/src/App.jsx
@@ -4,12 +4,14 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Setting from './pages/Setting';
+import Login from './pages/Login';
 
 export default function App() {
     return (
         <>
             <BrowserRouter>
                 <Routes>
+                    <Route path="/login" exact element={<Login />}></Route>
                     <Route path="/" exact element={<Home />}></Route>
                     <Route path="/about" exact element={<About />}></Route>
                     <Route path="/setting" exact element={<Setting />}></Route>
