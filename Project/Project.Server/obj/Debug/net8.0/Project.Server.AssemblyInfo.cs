1f80ac0ab5fc92814357294e373281dbf06babae
Clément BADRONE <clement.badrone@epitech.eu>
2024-10-14 15:33:52 +0200

    Push Main (test)

--- Project/Project.Server/obj/Debug/net8.0/Project.Server.AssemblyInfo.cs
+++ Project/Project.Server/obj/Debug/net8.0/Project.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d15e9ff246b1c3aeeeac4c859353baf810d1c30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86e4920a2a8cfb2cbbf5d58f16572a63565103fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
